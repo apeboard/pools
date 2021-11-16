8b68f84c585c13fe69f33e648b908d1b239fc307
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2021-11-16 13:33:50 +0700

    Add env condition

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -7,11 +7,22 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const pages = await new Promise((resolve, reject) => {
-    glob('**/*.ts', { cwd: './pages/api/' }, (err, pages) => {
+    const pattern =
+      process.env.NODE_ENV === 'development' ? '**/*.ts' : '**/*.js'
+
+    const cwd =
+      process.env.NODE_ENV === 'development'
+        ? './pages/api/'
+        : './.next/server/pages/api'
+
+    const indexFile =
+      process.env.NODE_ENV === 'development' ? 'index.ts' : 'index.js'
+
+    glob(pattern, { cwd }, (err, pages) => {
       if (err) reject(err)
       resolve(
         chain(pages)
-          .filter((page) => page !== 'index.ts')
+          .filter((page) => page !== indexFile)
           .reduce((acc, cur) => {
             const path = cur.replace(/(\.ts)/g, '').replace(/\//g, '.')
 
