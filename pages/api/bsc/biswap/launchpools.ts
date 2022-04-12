b711a8dcfdf49548962fa39c26feab6f51a1a342
jitinics <jitinics@Gmail.com>
2022-04-12 10:11:27 +0700

    Update launchpools.ts

--- pages/api/bsc/biswap/launchpools.ts
+++ pages/api/bsc/biswap/launchpools.ts
@@ -34,6 +34,46 @@ const launchpools: Launchpool[] = [
     name: 'Earn LOA',
     address: '0xcC1B012ad72736653e1e5043D902A4081F3f6a73',
   },
+  {
+    name: 'Earn BSW',
+    address: '0xa4b20183039b2F9881621C3A03732fBF0bfdff10',
+  },
+  {
+    name: 'Earn BSW(Auto)',
+    address: '0xcC1B012ad72736653e1e5043D902A4081F3f6a73',
+  },
+  {
+    name: 'Earn BSW',
+    address: '0xDbc1A13490deeF9c3C12b44FE77b503c1B061739',
+  },
+  {
+    name: 'Earn NEAR',
+    address: '0x6c7F1A28c64E09671beE2554e2bf40548bc7FE1f',
+  },
+  {
+    name: 'Earn SOL',
+    address: '0x8277f64fD5784325c29126518171D05298EC86a1',
+  },
+  {
+    name: 'Earn ADA',
+    address: '0xB535e0A88912fA434304E072Ae1f1Cb5bC814798',
+  },
+  {
+    name: 'Earn ETH',
+    address: '0xF2a99CA15082daD5ADFbBD3B5f976ed94e92143d',
+  },
+  {
+    name: 'Earn BTC',
+    address: '0x08a9a13701B7afBDE6716D10A57989A9533e0cfd',
+  },
+  {
+    name: 'Earn bMVL',
+    address: '0x9819339F6806A9C6C798bc2D09a309F693AC783D',
+  },
+  {
+    name: 'Earn GQ',
+    address: '0x0e2ab69B12B2D5036EA00C04eE213f8792f2EAF6',
+  },
 ]
 
 export default function handler(
